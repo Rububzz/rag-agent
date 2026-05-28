1370b28664be30a406d1b18bb945bed8146ea9d4
Rububzz <linrubin2@gmail.com>
2026-05-28 16:49:06 +0800

    improve query rewriting prompt to generate diverse query types

--- app/query_rewriter.py
+++ app/query_rewriter.py
@@ -5,7 +5,7 @@ from app.llm import groqAi
 logger = logging.getLogger(__name__)
 
 
-def rewrite_query(question: str, n: int) -> list[str]:
+def rewrite_query(question: str, n: int = 4) -> list[str]:
     try:
         logger.info(f"Calling Groq to give alternative questions")
         response = groqAi.chat.completions.create(
@@ -13,11 +13,11 @@ def rewrite_query(question: str, n: int) -> list[str]:
             messages=[
                 {
                     "role": "system",
-                    "content": f"Generate exactly {n} alternative phrasings of the user's question to improve document retrieval. Return one phrasing per line with no numbering, bullets, or extra text.",
+                    "content": f"Generate exactly 4 alternative queries using the question given. 1 being a semantic paraphrase, 2 being a keyword only search query, 3 being a broader contextual question and 4 being a technical terminolgy version of the question. Return one phrasing per line with no numbering, bullets, or extra text.",
                 },
                 {
                     "role": "user",
-                    "content": f"Give me a list of {n} number of alternative phrasings of this question: {question}",
+                    "content": question,
                 },
             ],
         )
@@ -26,7 +26,7 @@ def rewrite_query(question: str, n: int) -> list[str]:
             line.strip() for line in raw_questions.strip().split("\n") if line.strip()
         ]
         filtered = questions[:n]
-        logger.info(f"Generated {len(filtered)} number of alternative phrasings")
+        logger.info(f"Rewritten queries: {filtered}")
         return filtered
     except Exception as e:
         logger.error(f"Failed to call Groq for rewrite:{e}")
